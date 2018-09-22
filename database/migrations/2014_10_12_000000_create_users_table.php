9b2f738c2ece8ace4aa123b839fc73b0647cd797
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2018-09-22 19:33:17 -0300

    ajuste varchar email migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('lastname')->default('');
             $table->string('phone')->default('');
-            $table->string('email')->unique();
+            $table->string('email', 254)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
